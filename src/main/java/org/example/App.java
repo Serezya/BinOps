028abb6ae79d90c728faa97e603a29fdfb314f4b
Sergey <derbin.s-a@yandex.ru>
2020-08-23 13:05:14 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10", "11"));
+        System.out.println(bins.mult("10", "11"));
+    }
+}
